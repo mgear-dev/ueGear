a1cce2c64ac500269a587238aacd79effccd9316
Tomi Poveda <tpovedatd@gmail.com>
2022-08-06 03:05:41 +0200

    Basic ueGear Unreal Plugin structure

---
+++ Plugins/ueGear/Source/ueGear/Private/ueGear.cpp
@@ -0,0 +1,25 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ueGear.h"
+
+#define LOCTEXT_NAMESPACE "FueGearModule"
+
+void FueGearModule::StartupModule()
+{
+	FString fs = FPaths::ConvertRelativePathToFull(FPaths::GetPath("../"));
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *fs);
+
+	PluginSettings
+	
+}
+
+void FueGearModule::ShutdownModule()
+{
+	FString fs = FPaths::GetPath("../");
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *fs);
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *FPaths::ConvertRelativePathToFull(fs));
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FueGearModule, ueGear)
---
+++ Source/ueGearPlugin/ueGearPluginGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ueGearPluginGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEGEARPLUGIN_API AueGearPluginGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
