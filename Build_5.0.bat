3df1cc31ce25a241c8acf5d3b125cf5408caba08
Tomi Poveda <tpovedatd@gmail.com>
2022-08-22 02:08:52 +0200

    Added .bat files to Build Plugin package

---
+++ Build_5.0.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Epic Games\UE_5.0\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="D:\tools\dev\ueGear\Plugins\ueGear\ueGear.uplugin" -Package="D:\tools\dev\ueGear\Package\ueGear Plugin 5.0" -Rocket -VS2019
---
+++ Build_5.0_open.bat
@@ -0,0 +1,1 @@
+cmd /k "Build_5.0.bat"
