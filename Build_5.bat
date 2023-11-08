1e1a225ecc7faa7648c3c8e8b47bfedf7ff13171
miquelcampos <miquel.campos@gmail.com>
2023-11-08 10:16:54 +0900

    ueGear Plugin Setup. Update Build_5.bat  #146

--- Build_5.bat
+++ Build_5.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
 
 REM The following builds the plugin for
 REM - Unreal 5.2
@@ -11,19 +11,20 @@ if "%OS%"=="Windows_NT" (
 
     REM Get the current directory
     set "rootPath=%CD%"
-    echo Current directory is: %rootPath%
+    echo Current directory is: !rootPath!
 
     REM Build 5.2
     if exist "C:\Program Files\Epic Games\UE_5.2" (
-        call "C:\Program Files\Epic Games\UE_5.2\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="%rootPath%\Plugins\ueGear\ueGear.uplugin" -Package="%rootPath%\Package\ueGear_5.2" -Rocket -VS2019 || echo Failed to find Unreal 5.2
+        call "C:\Program Files\Epic Games\UE_5.2\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="!rootPath!\Plugins\ueGear\ueGear.uplugin" -Package="!rootPath!\Package\ueGear_5.2" -Rocket -VS2019 || echo Failed to find Unreal 5.2
     )
 
     REM Build 5.3
     if exist "C:\Program Files\Epic Games\UE_5.3" (
-        call "C:\Program Files\Epic Games\UE_5.3\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="%rootPath%\Plugins\ueGear\ueGear.uplugin" -Package="%rootPath%\Package\ueGear_5.3" -Rocket -VS2019 || echo Failed to find Unreal 5.3
+        call "C:\Program Files\Epic Games\UE_5.3\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="!rootPath!\Plugins\ueGear\ueGear.uplugin" -Package="!rootPath!\Package\ueGear_5.3" -Rocket -VS2019 || echo Failed to find Unreal 5.3
     )
 ) else (
     echo This script is designed for Windows only.
 )
 
 endlocal
+pause
